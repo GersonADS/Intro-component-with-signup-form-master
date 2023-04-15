61e37b7d4ca2439d73c15b95836858a76b5f4568
GersonADS <shiranuime@gmail.com>
2023-04-14 21:17:09 -0300

    finishing js

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -5,6 +5,7 @@ let $lname =document.querySelector('#lname');
 let $Email = document.querySelector('#email');
 let $Password =document.querySelector('#password');
 
+
 //BTN
 
 //botao teste 
@@ -15,18 +16,28 @@ let $butao = document.querySelector('#_button');
 let $Button = document.querySelector('#button');
 
 $butao.addEventListener('click',()=>{
-
-let inputs = [$Name,$lname,$Email,$Password];
-let values = [$Name.value,$lname.value,$Email.value,$Password.value];
+    
+    let inputs = [$Name,$lname,$Email,$Password];
+    let values = [$Name.value,$lname.value,$Email.value,$Password.value];
 
     for(let i = 0;i<=4;i++){
+
         if(!values[i]){
             inputs[i].style.backgroundImage = "url('/images/icon-error.svg')";
             inputs[i].style.backgroundRepeat ='no-repeat';
             inputs[i].style.backgroundPosition = '96%';
+
+            
+            let $Alert = document.querySelectorAll('.advert')[i];
+            $Alert.innerHTML = 'aaaaaaaaa'
+
         }else{
             inputs[i].style.backgroundImage = "";
-        }   
-     }
+            let $Alert = document.querySelectorAll('.advert')[i];
+            $Alert.innerHTML = '';
+        };
+
+    }
+
 
 })
